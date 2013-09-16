68ae2aa5fd3963ebc899bbd04fe7bb4ed5e7fb89
Mantas Norvaiša <mntnorv@gmail.com>
2013-09-16 21:54:42 +0300

    Added the very first chat UI prototype

--- public/index.php
+++ public/index.php
@@ -17,6 +17,10 @@
 	<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen" />
 	<link href="/css/main.css" rel="stylesheet" media="screen" />
 
+	<?php if ($logged_in): ?>
+		<link href="/css/chat.css" rel="stylesheet" media="screen" />
+	<?php endif ?>
+
 	<!-- Javascript -->
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
--- views/chat.php
+++ views/chat.php
@@ -1,3 +1,107 @@
 <div class="container">
-	Chat here
+	<div id="chat-log">
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+		<strong>test_user</strong>: Hey another_test_user! <br />
+		<strong>another_test_user</strong>: Hi back test_user! <br />
+	</div>
+</div>
+
+<div id="chat-input">
+	<div class="container">
+		<form>
+			<div class="input-group">
+				<input class="form-control" type="text" placeholder="Type a message..." />
+				<span class="input-group-btn">
+					<button class="btn btn-primary" type="submit">Send</button>
+				</span>
+			</div>
+		</form>
+	</div>
 </div>
