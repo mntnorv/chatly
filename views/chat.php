539b231c147ac1b3ed637f03c9a1fac072ca1bc5
Mantas Norvaiša <mntnorv@gmail.com>
2013-09-27 19:15:37 +0300

    Added content to the sidebar

--- views/chat.php
+++ views/chat.php
@@ -3,66 +3,29 @@
 		<div class="row fullheight-sm">
 			<div class="col-sm-3 fullheight-sm">
 				<div id="chat-sidebar-container">
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
-					Content <br />
+					<h4>Chat rooms</h4>
+					
+					<div id="chat-rooms">
+					</div>
+
+					<a class="btn btn-success btn-xs" href="#">Create new room</a>
+
+					<h4>Contacts</h4>
+					
+					<div class="contact-list">
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 1</a>
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 2</a>
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 3</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 4</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 5</a>
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 6</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 7</a>
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 8</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 9</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 10</a>
+						<a class="contact-list-item" href="#"><span class="status-offline">⬤ </span>Kontaktas 11</a>
+						<a class="contact-list-item" href="#"><span class="status-online">⬤ </span>Kontaktas 12</a>
+					</div>
 				</div>
 			</div>
 			<div class="col-sm-9 fullheight-sm">
